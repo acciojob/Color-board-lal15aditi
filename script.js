fe56515e3b0a3a3c39c4dd234686a48c6fe57b9d
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2024-05-17 21:20:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+  const board = document.getElementById("board");
+
+  // Generate 800 boxes
+  for (let i = 0; i < 800; i++) {
+    const box = document.createElement("div");
+    box.classList.add("square");
+    board.appendChild(box);
+    // Add event listeners
+    box.addEventListener("mouseover", function () {
+      box.style.backgroundColor = getRandomColor();
+      setTimeout(() => {
+        box.style.backgroundColor = 'rgb(29, 29, 29)';
+      }, 1000);
+    });
+  }
+});
+
+// Function to generate random color
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
